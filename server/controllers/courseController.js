1f84224f2162edd91f648637f7acc8c81bbc7900
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-09-02 15:40:52 +0530

    all done in backend:

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -8,7 +8,19 @@ import { Stats } from '../models/Stats.js'
 
 export const getAllCourses = catachAsyncErrors(async (req, res, next) => {
 
-    const courses = await Course.find().select("-lectures");
+    const keyword = req.query.keyword || "";
+    const category = req.query.category || "";
+
+    const courses = await Course.find({
+        title:{
+            $regex:keyword,
+            $options:"i",
+        },
+        category:{
+            $regex:category,
+            $options:"i",
+        },
+    }).select("-lectures");
 
     res.status(200).json({
         success: true,
